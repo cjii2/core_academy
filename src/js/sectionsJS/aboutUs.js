4c89c5335cfb78f0af731590cc8a352167d50673
cjii2 <ahmed12345m3@gmail.com>
2026-02-04 12:20:46 +0300

    update

--- src/js/sectionsJS/aboutUs.js
+++ src/js/sectionsJS/aboutUs.js
@@ -3,6 +3,7 @@ import {insertDataIntoInputs} from '../../main.js'
 export function about(){
     const elemnt = document.querySelectorAll('#about .all-about-content h2, .all-about-content p');
     const imgs = document.querySelectorAll('#about .img-container img');
+    const video = document.querySelectorAll('#about .img-container video');
     const url = 'src/json/aboutSection/insertData.json';
 
 
@@ -10,5 +11,6 @@ export function about(){
 
     about.getData('img').then(data=>about.inserDataIntoImg(data,imgs));
     about.getData('content').then(data=>about.insertDataIntoTags(data,elemnt));
+    about.getData('video').then(data=>about.inserDataIntoVideo(data,video));
     
 }
